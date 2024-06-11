7f807a62d16b6c1f446ecb9bcfdf8e942276c863
UdayasankaraReddyN <nudaysankarreddy@gmail.com>
2024-06-11 20:05:51 +0530

    variable data type

---
+++ variable.py
@@ -0,0 +1,17 @@
+a = 3
+print(3)
+
+name = "uday"
+
+print(name)
+
+b, c, d = 5, 6.4, "great"
+
+# print("b value is"+b) it will print error
+# concatanation value
+
+print("{} {} ".format("The b value  is ", d))
+# knowing type of integer
+print(type(b))
+print(type(c))
+print(type(d))
