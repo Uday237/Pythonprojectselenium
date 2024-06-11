6bb8d72b5e921d8618b470f592699da92da9ff52
UdayasankaraReddyN <nudaysankarreddy@gmail.com>
2024-06-11 22:08:35 +0530

    List and data code

---
+++ Datatype.py
@@ -0,0 +1,19 @@
+# integer type
+a = 100
+print("The type of having varibale value", a, "is ", type(a))
+
+b=10.5
+print("The type of having varibale value", b, "is ", type(b))
+
+
+c= 100+3j
+print("The type of having varibale value", c, "is ", type(c))
+
+d = "Hello Uday"
+e= 'Hello reddy'
+print(d)
+print(e)
+
+print(d, "Concanated with", e)
+
+print(d+ "Concanated with" +e)
---
+++ list.py
@@ -0,0 +1,19 @@
+values = [1, 2, 3, "Uday", 5]
+# list is the data type that allows multiple values
+
+print(values[0], print(type(values)))
+print(values[2], print(type(values)))
+
+print(values[-1])
+print(values[1:4])
+values.insert(3, "reddy")
+print(values)
+values.append("End")
+print(values)
+values[2] = "RAHUL"
+del values[0]
+print(values)
+
+
+
+
